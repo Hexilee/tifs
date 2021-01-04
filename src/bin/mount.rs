d9f3d0d4a408c324844843463d48bc833185fa9d
Wu Yufei <me@tespent.cn>
2021-01-04 10:06:57 +0800

    implement mount(8) interface

--- src/bin/mount.rs
+++ src/bin/mount.rs
@@ -2,34 +2,32 @@ use clap::{crate_version, App, Arg};
 use tracing_subscriber::EnvFilter;
 
 use tifs::MountOption;
-use tifs::mount_tifs;
+use tifs::mount_tifs_daemonize;
+//use daemonize::Daemonize;
 
 #[async_std::main]
 async fn main() {
-    let matches = App::new("TiFS")
+    let matches = App::new("mount.tifs")
         .version(crate_version!())
         .author("Hexi Lee")
         .arg(
-            Arg::with_name("pd")
-                .long("pd-endpoints")
-                .multiple(true)
+            Arg::with_name("device")
                 .value_name("ENDPOINTS")
-                .default_value("127.0.0.1:2379")
-                .help("set all pd endpoints of the tikv cluster")
-                .takes_value(true),
+                .required(true)
+                .help("all pd endpoints of the tikv cluster, separated by commas (e.g. tifs:127.0.0.1:2379)")
+                .index(1)
         )
         .arg(
             Arg::with_name("mount-point")
-                .long("mount-point")
                 .value_name("MOUNT_POINT")
                 .required(true)
                 .help("Act as a client, and mount FUSE at given path")
-                .takes_value(true),
+                .index(2)
         )
         .arg(
             Arg::with_name("options")
                 .value_name("OPTION")
-                .long("option")
+                .short("o")
                 .multiple(true)
                 .help("filesystem mount options")
         )
@@ -40,10 +38,14 @@ async fn main() {
         .try_init()
         .unwrap();
 
-    let endpoints: Vec<&str> = matches
-        .values_of("pd")
-        .unwrap_or_default()
-        .to_owned()
+    let device = matches
+        .value_of("device")
+        .unwrap_or_default();
+
+    let endpoints: Vec<&str> = device
+        .strip_prefix("tifs:")
+        .unwrap_or(device)
+        .split(",")
         .collect();
 
     let mountpoint: String = matches.value_of("mount-point").unwrap().to_string();
@@ -51,5 +53,9 @@ async fn main() {
         .values_of("options")
         .unwrap_or_default());
 
-    mount_tifs(mountpoint, endpoints, options).await.unwrap();
+    mount_tifs_daemonize(mountpoint, endpoints, options, || {
+//        Daemonize::new().working_directory("/").start()?;
+
+        Ok(())
+    }).await.unwrap();
 }
---
+++ src/lib.rs
@@ -0,0 +1,94 @@
+#![feature(map_into_keys_values)]
+#![feature(async_closure)]
+
+mod fs;
+
+use fs::async_fs::AsyncFs;
+use fs::tikv_fs::TiFs;
+
+use paste::paste;
+use fuser::MountOption as FuseMountOption;
+
+macro_rules! define_options {
+    { $name: ident, [ $($newopt: ident),* $(,)? ], [ $($opt: ident),* $(,)? ] } =>
+    {
+        define_options!{ $name(FuseMountOption), [ $($newopt,)* ], [ $($opt,)* ]}
+    };
+    { $name: ident ($type: ident), [ $($newopt: ident),* $(,)? ], [ $($opt: ident),* $(,)? ] } =>
+    {
+        #[derive(Debug,Clone)]
+        pub enum $name {
+            Unknown(String),
+            $($opt,)*
+            $($newopt,)*
+        }
+        impl $name {
+            pub fn to_vec<'a, I: Iterator<Item=&'a str>>(iter: I) -> Vec<Self> {
+                iter.map(|v| v.split(',').map(|v| Self::from(v))).flatten().collect()
+            }
+            pub fn to_builtin<'a, I: Iterator<Item=&'a Self>>(iter: I) -> Vec<$type> {
+                iter.filter_map(|v| v.into_builtin()).collect()
+            }
+            pub fn into_builtin(&self) -> Option<$type> {
+                match self {
+                    $(Self::$opt => Some($type::$opt),)*
+                    _ => None,
+                }
+            }
+        }
+        paste! {
+            impl<T> From<T> for $name
+            where
+                T: ToString
+            {
+                fn from(v: T) -> Self {
+                    match &v.to_string() as &str {
+                        $(stringify!([<$opt:lower>]) => Self::$opt,)*
+                        $(stringify!([<$newopt:lower>]) => Self::$newopt,)*
+                        k => Self::Unknown(k.to_owned()),
+                    }
+                }
+            }
+        }
+    };
+}
+
+define_options!{ MountOption, [], [
+    Dev,
+    NoDev,
+    Suid,
+    NoSuid,
+    RO,
+    RW,
+    Exec,
+    NoExec,
+    DirSync,
+] }
+
+
+pub async fn mount_tifs_daemonize<F>(mountpoint: String, endpoints: Vec<&str>, options: Vec<MountOption>, make_daemon: F) -> anyhow::Result<()>
+where
+    F: FnOnce() -> anyhow::Result<()>
+{
+    let mut fuse_options = vec![
+        FuseMountOption::FSName(format!("tifs:{}", endpoints.join(","))),
+        FuseMountOption::AllowOther,
+        FuseMountOption::AutoUnmount,
+        FuseMountOption::DefaultPermissions,
+    ];
+
+    fuse_options.extend(MountOption::to_builtin(options.iter()));
+
+    let fs_impl = TiFs::construct(endpoints, Default::default())
+        .await?;
+
+    make_daemon()?;
+
+    fuser::mount2(AsyncFs::from(fs_impl), mountpoint, &fuse_options)?;
+
+    Ok(())
+}
+
+pub async fn mount_tifs(mountpoint: String, endpoints: Vec<&str>, options: Vec<MountOption>) -> anyhow::Result<()> {
+    mount_tifs_daemonize(mountpoint, endpoints, options, || {Ok(())}).await
+}
